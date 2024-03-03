9bbcb229042840ca23afa57a7cb0b11684060eef
Naresh <nareshtalesha03@gmail.com>
2024-03-02 23:50:27 -0600

    test

--- tests/excelDemo.py
+++ tests/excelDemo.py
@@ -4,3 +4,10 @@ book = openpyxl.load_workbook("/Users/ntalesha/Desktop/TESTING.xlsx")
 sheet = book.active
 cell = sheet.cell(row=1,column=2)
 print(cell.value)
+sheet.cell(row=2,column=2).value = "Rahul"
+
+print(sheet.cell(row=2,column=2).value)
+
+print(sheet.max_row)
+print(sheet.max_column)
+print(sheet['A5'].value)
